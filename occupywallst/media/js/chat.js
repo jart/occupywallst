e834ee7c650964ed8f3e4ed43bdfc72a75e28341
Justine Tunney <jtunney@lobstertech.com>
2011-07-22 07:04:08 -0400

    more chat stuff going on

---
+++ occupywallst/media/js/chat.js
@@ -0,0 +1,142 @@
+
+var chat_init;
+
+(function() {
+    "use strict";
+
+    var me = null;
+
+    function init(args) {
+        $("#msgtext").focus();
+        var chat = io.connect(args.url);
+        chat.on('connect', function () {
+            chat.emit("join", {room: "pub"});
+        });
+        chat.socket.on('error', function (reason) {
+            display({text: "error: " + reason});
+        });
+        chat.on("msg", display);
+        chat.on("join_ack", function (msg) {
+            me = msg.user;
+            display({text: "Welcome " + me.name + "!"});
+            display({text: "Joined: " + msg.room});
+            for (var k in msg.users) {
+                add_user(msg.users[k]);
+            }
+        });
+        chat.on("leave", function (msg) {
+            display({text: msg.user.name + " has left"});
+            remove_user(msg.user);
+        });
+        chat.on("leave_ack", function (msg) {
+            display({text: "you have left " + msg.room});
+            $("#users").empty();
+        });
+        chat.on("kicked", function (msg) {
+            display({text: "you were kicked from " + msg.room});
+        });
+        chat.on("join", function (msg) {
+            add_user(msg.user);
+            display({text: msg.user.name + " has joined"});
+        });
+        $("#postform").submit(function () {
+            if (!me)
+                return false;
+            var text = $("#msgtext").val();
+            $("#msgtext").val("");
+            if (text.length < 2)
+                return;
+            if (text[0] == "/") {
+                var command, args;
+                if (text.indexOf(" ") != -1) {
+                    command = text.slice(1, text.indexOf(' '));
+                    args = text.slice(text.indexOf(' ') + 1);
+                } else {
+                    command = text.slice(1);
+                    args = '';
+                }
+                if (command == "kick") {
+                    chat.emit("kick", {room: "pub", user: {name: args}});
+                } else if (command == "me") {
+                    var msg = {
+                        room: "pub",
+                        user: me,
+                        text: args,
+                        emo: true
+                    };
+                    display(msg);
+                    chat.emit("msg", msg);
+                } else {
+                    display({text: "unknown command"});
+                }
+            } else {
+                var msg = {
+                    room: "pub",
+                    user: me,
+                    text: text,
+                    emo: false
+                };
+                display(msg);
+                chat.emit("msg", msg);
+            }
+            return false;
+        });
+    }
+
+    function display(msg) {
+        var line = $("<div/>");
+        if (msg.user) {
+            if (msg.emo) {
+                line
+                .append($("<span/>").text(curtime()))
+                .append($("<span> &nbsp;* </span>"))
+                .append(fun_username(msg.user.name))
+                .append($("<span/>").text(" " + msg.text));
+            } else {
+                line
+                .append($("<span/>").text(curtime() + " "))
+                .append($("<span/>").text("<"))
+                .append(fun_username(msg.user.name))
+                .append($("<span/>").text(">"))
+                .append($("<span/>").text(" " + msg.text));
+            }
+        } else {
+            line.text(curtime() + " -!- " + msg.text);
+        }
+        $("#messages").append(line);
+        $("#messages").prop({scrollTop: $("#messages").prop("scrollHeight")});
+    }
+
+    function add_user(user) {
+        $("#users").append($("<div/>").append(fun_username(user.name)));
+    }
+
+    function fun_username(name) {
+        if (name.match(/^anon[0-9]+$/)) {
+            return $("<span/>").text(name);
+        } else {
+            return $("<a/>").attr("href", "/users/" + name + '/').text(name);
+        }
+    }
+
+    function remove_user(user) {
+        $("#users div").each(function () {
+            if ($(this).text() == user.name) {
+                $(this).remove();
+            }
+        });
+    }
+
+    function curtime() {
+        var now = new Date();
+        var hours = now.getHours();
+        var minutes = now.getMinutes();
+        if (hours < 10) hours = "0" + hours;
+        if (minutes < 10) minutes = "0" + minutes;
+        return "" + hours + ":" + minutes;
+    }
+
+    // export stuff
+    chat_init = init;
+
+})();
