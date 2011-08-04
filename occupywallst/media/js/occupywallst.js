43bc0f90be05aaf0442d29089b0852ad28de99af
Justine Tunney <jtunney@lobstertech.com>
2011-08-03 22:41:44 -0400

    middle clicking forum links should open in new tab

--- occupywallst/media/js/occupywallst.js
+++ occupywallst/media/js/occupywallst.js
@@ -4,11 +4,21 @@ var occupywallst_init;
 (function() {
     "use strict";
 
+    const MOUSE_LEFT = 1;
+    const MOUSE_MIDDLE = 2;
+    const MOUSE_RIGHT = 3;
+
     function init() {
-        $(".clickdiv, .clickdiv a").click(function() {
-            window.location.href = ($(this).attr('href') ||
-                                    $("a.primary", this).attr('href'));
-            return false;
+        $(".clickdiv, .clickdiv a").click(function(e) {
+            var url = ($(this).attr('href') ||
+                       $("a.primary", this).attr('href'));
+            if (e.which == MOUSE_LEFT) {
+                window.location.href = url;
+                return false;
+            } else if (e.which == MOUSE_MIDDLE) {
+                window.open(url);
+                return false;
+            }
         });
     };
 
