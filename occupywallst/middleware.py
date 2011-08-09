9457a60cca61d86ed6c4c60f46f1cc397a90b7ed
Justine Tunney <jtunney@lobstertech.com>
2011-08-09 12:01:41 -0400

    don't cache stuff until i figure out how to handle caching properly

--- occupywallst/middleware.py
+++ occupywallst/middleware.py
@@ -9,7 +9,15 @@ r"""
 
 import traceback
 
+from django.utils.cache import add_never_cache_headers
+
 
 class PrintException(object):
     def process_exception(self, request, exception):
         traceback.print_exc()
+
+
+class NeverCache(object):
+    def process_response(self, request, response):
+        add_never_cache_headers(response)
+        return response
--- occupywallst/settings.py
+++ occupywallst/settings.py
@@ -154,6 +154,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'occupywallst.middleware.NeverCache',
 ]
 
 INSTALLED_APPS = [
