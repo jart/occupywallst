ae68c2d888552757271d1bde928f0ded7d6caad2
abie <abie@riseup>
2011-11-25 20:03:44 -0800

    make link distance related to time between posts

--- viz/force.js
+++ viz/force.js
@@ -1,8 +1,8 @@
 var w = 960,
     h = 500,
     fill = d3.scale.linear()
-      .domain([0,5,10,Infinity])
-      .range(["green", "yellow", "red", "red"]);
+      .domain([-1,0,5,10,Infinity])
+      .range(["white", "green", "yellow", "red", "red"]);
 
 var vis = d3.select("#chart").append("svg:svg")
     .attr("width", w)
@@ -11,12 +11,12 @@ var vis = d3.select("#chart").append("svg:svg")
 d3.json("t.json", function(json) {
   // create nodes and node index
   var ix = {null:0};
-  var nodes = [{"name": "article", "ups": 25, "downs": 0}];
+  var nodes = [{"content": "(original article)", "ups": 25, "downs": -1, "published": json.results[0].published-1.0}];
   var i = 1;
   for (r in json.results) {
-      nodes.push(json.results[r]);
-      var id = json.results[r].id;
-      ix[id] = i;
+      n = json.results[r];
+      nodes.push(n);
+      ix[n.id] = i;
       i++;
   }
 
@@ -28,10 +28,15 @@ d3.json("t.json", function(json) {
       links.push({"source":ix[p_id], "target":ix[id], "weight":1});
   }
 
+  var link_distance = function(link) {
+      d = (link.target.published - link.source.published);
+      return 1.0 + Math.log(1.0 + Math.abs(d/1000.0));
+  }
+
   var force = d3.layout.force()
-      .charge(-10)
+      .charge(-25)
       .friction(.9)
-      .linkDistance(3)
+      .linkDistance(link_distance)
       .nodes(nodes)
       .links(links)
       .size([w, h])
@@ -53,12 +58,12 @@ d3.json("t.json", function(json) {
       .attr("class", "node")
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
-      .attr("r", function(d) { return d.ups; })
+      .attr("r", function(d) { return d.ups+1; })
       .style("fill", function(d) { return fill(d.downs); })
       .call(force.drag);
 
   node.append("svg:title")
-      .text(function(d) { return d.name; });
+      .text(function(d) { return d.content; });
 
   force.on("tick", function() {
     link.attr("x1", function(d) { return d.source.x; })
